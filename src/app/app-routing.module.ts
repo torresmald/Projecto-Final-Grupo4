b5348758efa33a9d74dffc90b5228bd627a778e9
beapdp <bea.palomo.delpozo@gmail.com>
2023-03-16 15:51:25 +0100

    rutas agregadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,6 +20,34 @@ const routes: Routes = [
     path: 'students',
     loadChildren: () => import('../app/pages/students/students.module').then(m => m.StudentsModule),
   }, 
+  {
+    path: 'teacherView',
+    loadChildren: () => import('../app/pages/teacher-view/teacher-view.module').then(m => m.TeacherViewModule),
+  },
+  {
+    path: 'familyView',
+    loadChildren: () => import('../app/pages/family-view/family-view.module').then(m => m.FamilyViewModule),
+  },
+  {
+    path: 'studentDetail',
+    loadChildren: () => import('../app/pages/student-detail/student-detail.module').then(m => m.StudentDetailModule),
+  },
+  {
+    path: 'teacher-messages',
+    loadChildren: () => import('../app/pages/messages-teachers/messages-teachers.module').then(m => m.MessagesTeachersModule),
+  },
+  {
+    path: 'notifications',
+    loadChildren: () => import('../app/pages/family-notifications/family-notifications.module').then(m => m.FamilyNotificationsModule),
+  },
+  {
+    path: 'resources',
+    loadChildren: () => import('../app/pages/family-resources/family-resources.module').then(m => m.FamilyResourcesModule),
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('../app/pages/family-contact/family-contact.module').then(m => m.FamilyContactModule),
+  },
 
   {
     path: '**',
