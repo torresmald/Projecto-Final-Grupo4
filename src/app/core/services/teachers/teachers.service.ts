f10913909b40d5cc55139dbe3cdbe359579f4c6f
Jonathan Torres <jonathan.torresmald@gmail.com>
2023-03-19 00:25:05 +0100

    feat: backtoTopButton

--- src/app/core/services/teachers/teachers.service.ts
+++ src/app/core/services/teachers/teachers.service.ts
@@ -11,8 +11,17 @@ const TOKEN_KEY = 'user-token-key';
 export class TeachersService {
   public userLogged$: ReplaySubject<boolean> = new ReplaySubject<boolean>(1);
 
-  constructor(private teachersService: ApiTeachersService) { }
-
+  constructor(private teachersService: ApiTeachersService) { 
+    this.userLogged$.next(this.isLogged());
+  }
+  public isLogged(): boolean {
+    const authToken = localStorage.getItem(TOKEN_KEY);
+    if (!authToken) {
+      return false;
+    }
+    const isValidToken = JSON.parse(authToken).token;
+    return !!isValidToken;
+  }
   public loginTeacher(body: ApiTeachers): Observable<ApiUsers> {
     return this.teachersService.loginApiTeacher(body).pipe(
       tap((response: ApiUsers) => {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlertComponent } from './alert/alert.component';
-
-
+import { ScrollToTopComponent } from './scroll-to-top/scroll-to-top.component';
 
 @NgModule({
-  declarations: [
-    AlertComponent
-  ],
-  imports: [
-    CommonModule
-  ],
+  declarations: [AlertComponent, ScrollToTopComponent],
+  imports: [CommonModule],
 
-  exports: [
-
-    AlertComponent
-  ]
+  exports: [ScrollToTopComponent, AlertComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
