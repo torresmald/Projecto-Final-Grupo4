246c0e2ecbab03066aa26b703c20d44b20e28338
macumota <maria.inmaculada.mota@gmail.com>
2023-03-20 23:38:16 +0100

    feat: componentes contact y privacity creados

---
+++ src/app/pages/privacity/privacity.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PrivacityComponent } from './privacity.component';
+
+
+
+@NgModule({
+  declarations: [
+    PrivacityComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PrivacityModule { }
