60ca2c320c128407b8a8272a313c1349cf7cdad1
beapdp <bea.palomo.delpozo@gmail.com>
2023-03-21 18:37:16 +0100

    vista videos youtube

--- src/app/pages/family-resources/family-resources.module.ts
+++ src/app/pages/family-resources/family-resources.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { FamilyResourcesRoutingModule } from './family-resources-routing.module';
 import { FamilyResourcesComponent } from './family-resources.component';
 import { ResourceComponent } from './resource/resource.component';
+import { SongsComponent } from './songs/songs.component';
 
 
 @NgModule({
   declarations: [
     FamilyResourcesComponent,
-    ResourceComponent
+    ResourceComponent,
+    SongsComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/pages/family-resources/songs/songs.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Resources } from '../../../core/models/Resources/transformed/resources.model';
+import { ResourcesService } from 'src/app/core/services/resources/resources.service';
+
+interface SongResource extends Resources {
+  safeLink: SafeResourceUrl;
+}
+
+@Component({
+  selector: 'app-songs',
+  templateUrl: './songs.component.html',
+  styleUrls: ['./songs.component.scss']
+})
+export class SongsComponent implements OnInit {
+  songs: SongResource[] = [];
+
+  constructor(private resourcesService: ResourcesService, private sanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+    this.resourcesService.getResources().subscribe((resources: Resources[]) => {
+      this.songs = resources.filter(resource => resource.type === 'songs').map(song => {
+        return {
+          ...song,
+          safeLink: this.getSafeUrl(song.link)
+        } as SongResource;
+      });
+    });
+  }
+
+  getSafeUrl(url: string): SafeResourceUrl {
+    const youtubeEmbedUrl = url.replace('watch?v=', 'embed/');
+    return this.sanitizer.bypassSecurityTrustResourceUrl(youtubeEmbedUrl);
+  }
+}
