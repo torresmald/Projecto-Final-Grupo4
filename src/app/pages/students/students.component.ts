0ee2b850499a1ac7b8c601f38413110ca7cdb699
Jonathan Torres <jonathan.torresmald@gmail.com>
2023-03-22 08:15:09 +0100

    try liststudents Pdf

--- src/app/pages/students/students.component.ts
+++ src/app/pages/students/students.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Students } from 'src/app/core/models/Students/transformed/students.model';
 import { StudentsService } from 'src/app/core/services/students/students.service';
+import { jsPDF } from 'jspdf';
+import autoTable from 'jspdf-autotable';
 const TOKEN_KEY = 'user-token-key';
 
 
@@ -12,10 +14,11 @@ const TOKEN_KEY = 'user-token-key';
 })
 export class StudentsComponent {
 
-
     public students: Students[] = [];
     public grade?: string[];
     public token?: string[];
+    public img = new Image();
+    public studentImg: any = new Image();
     constructor(private studentsService: StudentsService, private router: Router) {
       const authToken = localStorage.getItem(TOKEN_KEY);
       authToken ? (this.token = JSON.parse(authToken).user.grade) : null;
@@ -30,7 +33,63 @@ export class StudentsComponent {
     public goBack(): void{
       this.router.navigate(['teacherView']);
     }
-    
+
+    public generatePDF(student?: Students[]) {
+      this.img.src = '../../../assets/logos/LogoGeneral.png';
+      const doc = new jsPDF();
+      for (let index = 0; index < this.students.length; index++) {
+        let student = this.students[index];
+        autoTable(doc, {
+          body: [
+            [
+              {
+                content: 'COLEGIO SEMILLA',
+                styles: {
+                  halign: 'left',
+                  fontSize: 18,
+                  textColor: 'white',
+                },
+              },
+              {
+                content: `${student?.name}`,
+                styles: {
+                  halign: 'right',
+                  fontSize: 18,
+                  textColor: 'white',
+                },
+              },
+            ],
+          ],
+          theme: 'plain',
+          styles: {
+            fillColor: `rgb(255, 87, 87)`,
+          },
+        });
+        autoTable(doc, {
+          body: [
+            [
+              {
+                content: `
+             Direccion: ${student.address}
+             Telefono:${student?.phone} 
+             Año de nacimiento: ${student?.date}
+             Clase: ${student?.grade}
+             Alergias: ${student?.diseases}
+             Medicamentos: ${student?.nutrition}    
+             Areas:  ${student?.areas}
+            `,
+                styles: {
+                  halign: 'left',
+                },
+              },
+            ],
+          ],
+          theme: 'grid',
+        });
+        doc.addImage(this.img, 'png',100,5,40,30);
+      } 
+      return doc.save('prueba');
+    }
   }
   
 
