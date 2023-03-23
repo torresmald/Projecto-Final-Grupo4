ca19739b75ce48a50858177d03b7335fe390cd5a
josegs72 <josegs72@gmail.com>
2023-03-23 14:34:12 +0100

    calendar local

--- src/app/pages/teacher-view/event-utils.ts
+++ src/app/pages/teacher-view/event-utils.ts
@@ -1,14 +1,18 @@
-import { EventInput } from '@fullcalendar/core';
+
+import { EventInput, Calendar } from '@fullcalendar/core';
+
 
 let eventGuid = 0;
-const TODAY_STR = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
+const TODAY_STR = new Date().toISOString().replace(/T.*$/, '');
+
+
 
 export const INITIAL_EVENTS: EventInput[] = [
   {
     id: createEventId(),
     title: 'Ir a la universidad',
     start: TODAY_STR,
-    display: 'background'
+    display: 'background',
   },
   {
     id: createEventId(),
@@ -22,10 +26,42 @@ export const INITIAL_EVENTS: EventInput[] = [
     id: createEventId(),
     title: 'Reunión',
     start: TODAY_STR + 'T12:00:00',
-    end: TODAY_STR + 'T15:00:00'
+    end: TODAY_STR + 'T15:00:00',
+  },
+  {
+    title: 'Conferencia sobre la vida',
+    start: '2023-03-11',
+    end: '2023-03-13',
+    color: '',
+    textColor: 'black',
+  },
+  {
+    title: 'Reconocimiento medico',
+    start: '2023-03-25',
+    end: '2023-03-27',
+    color: 'red',
+    textColor: 'black',
+  },
+
+ 
+
+  {
+    title: 'Click for Google-calendar',
+    url: 'https://calendar.google.com/calendar/u/0/r/',
+    start: '2023-04-01',
+    end: '2023-04-02',
+    color: 'red',
+
+
   }
 ];
 
+
+
 export function createEventId() {
   return String(eventGuid++);
+
+
+
+
 }
--- src/app/pages/teacher-view/teacher-view.component.ts
+++ src/app/pages/teacher-view/teacher-view.component.ts
@@ -1,4 +1,5 @@
 
+
 import { ApiTeachers } from 'src/app/core/models/Teachers/api/api-teachers.model';
 import { Component, ChangeDetectorRef } from '@angular/core';
 import { CalendarOptions, DateSelectArg, EventClickArg, EventApi, Calendar,} from '@fullcalendar/core';
@@ -8,9 +9,16 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import { INITIAL_EVENTS, createEventId } from './event-utils';
 import esLocale from '@fullcalendar/core/locales/es';
+import iCalendarPlugin from '@fullcalendar/icalendar';
+import googleCalendarPlugin from '@fullcalendar/google-calendar';
+
+ 
+
+    
 
 
 const TOKEN_KEY = 'user-token-key';
+const EVENTS_KEY = 'events';
 
 @Component({
   selector: 'app-teacher-view',
@@ -22,19 +30,28 @@ export class TeacherViewComponent {
   constructor(private changeDetector: ChangeDetectorRef) {
     const authToken = localStorage.getItem(TOKEN_KEY);
     authToken ? (this.token = JSON.parse(authToken).user) : null;
+  
+  
+  const eventsJson = localStorage.getItem(EVENTS_KEY);
+  if (eventsJson) {
+    const events = JSON.parse(eventsJson);
+    this.calendarOptions.events = events;
   }
+}
 
   calendarVisible = true;
   calendarOptions: CalendarOptions = {
-    plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin],
+    plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin,iCalendarPlugin,googleCalendarPlugin],
     locale: esLocale,
     aspectRatio: 2,
     headerToolbar: {
       left: 'prev,next today',
       center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek, iCalendarPlugin',
       
     },
+   
+
     initialView: 'dayGridMonth',
     initialEvents: INITIAL_EVENTS, 
     weekends: true,
@@ -45,8 +62,18 @@ export class TeacherViewComponent {
     select: this.handleDateSelect.bind(this),
     eventClick: this.handleEventClick.bind(this),
     eventsSet: this.handleEvents.bind(this),
+    googleCalendarApiKey: 'AIzaSyBWfBZfBxSCJxlyDUMfr5-RUQr7O68Zf1k',
+    events: [
+      { 
+          googleCalendarId: '1067325408993-opda1p9fgfua22a00ei74do2rn5is6hj.apps.googleusercontent.com',
+          textColor: 'white',
+          borderColor: '#4caf50'
+        }
+      ]
+    };
     
-  };
+    
+    
   currentEvents: EventApi[] = [];
 
   handleCalendarToggle() {
@@ -62,29 +89,36 @@ export class TeacherViewComponent {
     const title = prompt('Porfavor ingrese el nombre del evento');
     const calendarApi = selectInfo.view.calendar;
 
-    calendarApi.unselect(); // clear date selection
+    calendarApi.unselect(); 
 
     if (title) {
-      calendarApi.addEvent({
+     const newEvent =({
         id: createEventId(),
         title,
         start: selectInfo.startStr,
         end: selectInfo.endStr,
         allDay: selectInfo.allDay,
+        url: 'https://calendar.google.com/calendar/u/0/r/',
       });
+      calendarApi.addEvent(newEvent);
+
+    const eventsJson = localStorage.getItem(EVENTS_KEY);
+    const events = eventsJson ? JSON.parse(eventsJson) : [];
+    localStorage.setItem(EVENTS_KEY, JSON.stringify([...events, newEvent]));
     }
   }
 
   handleEventClick(clickInfo: EventClickArg) {
-    if (
-      confirm(
-        `Estas seguro eliminar evento '${clickInfo.event.title}'`
-      )
+    if (clickInfo.event.url) {
+      window.open(clickInfo.event.url, '_blank');
+    } else if (
+      confirm(`Estas seguro eliminar evento '${clickInfo.event.title}'`)
     ) {
       clickInfo.event.remove();
     }
   }
-
+  
+  
   handleEvents(events: EventApi[]) {
     this.currentEvents = events;
     this.changeDetector.detectChanges();
@@ -92,4 +126,6 @@ export class TeacherViewComponent {
  
 
   
-}
+
+
+};
