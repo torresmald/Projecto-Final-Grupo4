3c29150d224b328db4261b98bc129852df513252
Jonathan Torres <jonathan.torresmald@gmail.com>
2023-03-27 17:56:20 +0200

    changed message view Styles

--- src/app/pages/messages-teachers/messages-teachers.component.ts
+++ src/app/pages/messages-teachers/messages-teachers.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { ApiStudents } from './../../core/models/Students/api/api-students.model';
 import { NotificationsService } from './../../core/services/notifications/notifications.service';
@@ -9,25 +14,32 @@ const TOKEN_KEY = 'user-token-key';
 @Component({
   selector: 'app-messages-teachers',
   templateUrl: './messages-teachers.component.html',
-  styleUrls: ['./messages-teachers.component.scss']
+  styleUrls: ['./messages-teachers.component.scss'],
 })
 export class MessagesTeachersComponent implements OnInit {
-
   public msgForm?: FormGroup;
   public students: ApiStudents[] = [];
-  public image :Blob | string="";
-  public calendar :Blob | string="";
+  public image: Blob | string = '';
+  public calendar: Blob | string = '';
   public token?: string[];
 
-  constructor (private msgBuilder: FormBuilder, private studentService: ApiStudentsService, private notificationService: NotificationsService, private router: Router) {
+  constructor(
+    private msgBuilder: FormBuilder,
+    private studentService: ApiStudentsService,
+    private notificationService: NotificationsService,
+    private router: Router
+  ) {
     const authToken = localStorage.getItem(TOKEN_KEY);
     authToken ? (this.token = JSON.parse(authToken).user.grade) : null;
   }
 
-
   ngOnInit(): void {
-    this.studentService.getAllStudens().subscribe((value) => this.students = value.filter((grade) => grade.grade === this.token
-    ))
+    this.studentService
+      .getAllStudens()
+      .subscribe(
+        (value) =>
+          (this.students = value.filter((grade) => grade.grade === this.token))
+      );
 
     this.msgForm = this.msgBuilder.group({
       name: new FormControl('', Validators.required),
@@ -35,39 +47,38 @@ export class MessagesTeachersComponent implements OnInit {
       description: new FormControl(''),
       image: new FormControl(null),
       calendar: new FormControl(null),
-      student: new FormControl(this.students)
-    })
-   
+      student: new FormControl(this.students),
+    });
   }
 
-  public newNotifications() {  
+  public newNotifications() {
     const form = new FormData();
-    form.append("name", this.msgForm?.get("name")?.value);
-    form.append("date", this.msgForm?.get("date")?.value);
-    form.append("description", this.msgForm?.get("description")?.value);
-    form.append("image", this.image);
-    form.append("calendar", this.calendar);
-    form.append("student", this.msgForm?.get("student")?.value);
-
+    form.append('name', this.msgForm?.get('name')?.value);
+    form.append('date', this.msgForm?.get('date')?.value);
+    form.append('description', this.msgForm?.get('description')?.value);
+    form.append('image', this.image);
+    form.append('calendar', this.calendar);
+    form.append('student', this.msgForm?.get('student')?.value);
 
     this.notificationService.postNotification(form).subscribe(() => {
-      alert('Enviado con exito');
-      this.msgForm?.reset();
-      this.router.navigate(['teacherView']);
-    })    
+      if(this.msgForm?.valid){
+        alert('Enviado con exito');
+        this.msgForm?.reset();
+        this.router.navigate(['teacherView']);
+      }
+    });
   }
-  public uploadImage(event:any) {
+  public uploadImage(event: any) {
     const reader1 = new FileReader();
     const reader2 = new FileReader();
 
-    if(event.target.files && event.target.files.length) {
+    if (event.target.files && event.target.files.length) {
       const file1 = event.target.files[0];
-      const file2 = event.target.files[0];      
-      reader1.readAsArrayBuffer(file1);  
-      reader2.readAsArrayBuffer(file2);   
-      this.image =file1;  
-      this.calendar = file2; 
+      const file2 = event.target.files[0];
+      reader1.readAsArrayBuffer(file1);
+      reader2.readAsArrayBuffer(file2);
+      this.image = file1;
+      this.calendar = file2;
+    }
   }
-}
-
 }
