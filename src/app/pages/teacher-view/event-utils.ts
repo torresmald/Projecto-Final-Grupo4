f16131961d44a0202c99ea004e9ac14f798d3f34
josegs72 <josegs72@gmail.com>
2023-03-28 20:42:07 +0200

    mas eventos y comentarios

--- src/app/pages/teacher-view/event-utils.ts
+++ src/app/pages/teacher-view/event-utils.ts
@@ -21,7 +21,28 @@ export const INITIAL_EVENTS: EventInput[] = [
     title: 'Reunión',
     start: TODAY_STR + 'T12:00:00',
     end: TODAY_STR + 'T15:00:00'
-  }
+  },
+  { id: createEventId(),
+     title: 'Claustro', 
+     start: '2023-03-08', 
+     end: '2023-03-10',
+     color: 'red',
+     allDay: true,
+    },
+  { id: createEventId(),
+      title: 'Reunión de padres',
+      start: '2023-03-20',
+      end: '2023-03-22',
+      color: 'green',
+      allDay: true,
+    },
+  { id: createEventId(),
+      title: 'Excursion escolar',
+      start: '2023-03-28',
+      end: '2023-03-28',
+      color: 'orange',
+      allDay: true,
+    },
 ];
 
 export function createEventId() {
